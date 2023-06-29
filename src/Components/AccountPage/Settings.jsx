1b75922168af2a8e51814929cd4a851c96fe3c8b
rakuj1n <you@example.com>
2023-06-29 15:55:43 +0800

    additional updates to settings change password function

--- src/Components/AccountPage/Settings.jsx
+++ src/Components/AccountPage/Settings.jsx
@@ -7,13 +7,14 @@ export default function Settings() {
         <main>
             <h3>Change My Profile Picture</h3>
             <form>
-                <label><input type='file'></input></label>
+                <label>Enter your image URL:<input type='url'></input></label>
                 <button>Upload</button>
             </form>
             <h3>Change My Password</h3>
             <form>
-                <label><input type='password'></input></label>
-                <button>Change</button>
+                <label>Current Password:<input type='password' name='confirm'></input></label>
+                <label>New Password:<input type='password' name='password'></input></label>
+                <button>Submit</button>
             </form>
         </main>
     )
