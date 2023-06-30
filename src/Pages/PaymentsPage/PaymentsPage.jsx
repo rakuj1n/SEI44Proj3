5cd4a952880eb902a7b7d68e3c04ddb37ed133a3
genotabby <teohwesley@gmail.com>
2023-06-30 12:08:38 +0800

    Feat: Payment will throw error if incorrectly accessed

--- src/Pages/PaymentsPage/PaymentsPage.jsx
+++ src/Pages/PaymentsPage/PaymentsPage.jsx
@@ -2,15 +2,22 @@ import { useLocation } from "react-router-dom";
 
 export default function PaymentsPage() {
   const { state } = useLocation();
+
   console.log(state);
 
   return (
-    <>
-      Payments page
-      <div>
-        {state.currency}
-        {state.price}
-      </div>
-    </>
+    <main className="payment_page">
+      {state === null ? (
+        <>error no payments</>
+      ) : (
+        <>
+          Payments page
+          <div>
+            {state.currency}
+            {state.price}
+          </div>
+        </>
+      )}
+    </main>
   );
 }
