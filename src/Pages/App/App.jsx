786fd194fb046574db517bd8d51f4d9f4e8c59ae
mindytyj <derpbritz@gmail.com>
2023-06-30 15:08:28 +0800

    Add Route Path for Log In Page and Sign Up Page

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from 'react';
-import './App.css'
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import AuthPage from '../AuthPage/AuthPage';
-import { Routes, Route } from 'react-router-dom';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import Navbar from '../../Components/Navbar';
-import { getUser } from '../../utilities/users-service';
-import MyFriendsPage from '../MyFriendsPage.jsx/MyFriendsPage';
-import AccountPage from '../AccountPage/AccountPage';
-import Settings from '../../Components/AccountPage/Settings';
-import Profile from '../../Components/AccountPage/Profile';
+import { useState } from "react";
+import "./App.css";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import AuthPage from "../AuthPage/AuthPage";
+import { Routes, Route } from "react-router-dom";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import Navbar from "../../Components/Navbar";
+import { getUser } from "../../utilities/users-service";
+import MyFriendsPage from "../MyFriendsPage.jsx/MyFriendsPage";
+import AccountPage from "../AccountPage/AccountPage";
+import Settings from "../../Components/AccountPage/Settings";
+import Profile from "../../Components/AccountPage/Profile";
+import LogInPage from "../LogInPage/LogInPage";
+import SignUpPage from "../SignUpPage/SignUpPage";
 
 export default function App() {
-
-  const [user,setUser] = useState(getUser())
-
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
-      {user ? 
-      <>
-        <Navbar user={user} setUser={setUser}/>
-        <Routes>
-          <Route path='/orders' element={<OrderHistoryPage />} />
-          <Route path='/orders/new' element={<NewOrderPage />} />
-          <Route path='/users/:userId/friends' element={<MyFriendsPage />} />
-          <Route path='/users/:userId' element={<AccountPage user={user}/>}>
-            <Route path='/users/:userId/settings' element={<Settings user={user}/>} />
-            <Route path='/users/:userId' element={<Profile user={user}/>} />
-          </Route>
-        </Routes>
-      </>
-      :
-      <AuthPage setUser={setUser}/>
-      }
+      {user ? (
+        <>
+          <Navbar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/users/:userId/friends" element={<MyFriendsPage />} />
+            <Route path="/users/:userId" element={<AccountPage user={user} />}>
+              <Route
+                path="/users/:userId/settings"
+                element={<Settings user={user} />}
+              />
+              <Route path="/users/:userId" element={<Profile user={user} />} />
+            </Route>
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser}>
+          <Route path="/" element={<LogInPage />} />
+          <Route path="/register" element={<SignUpPage />} />
+        </AuthPage>
+      )}
     </main>
   );
 }
