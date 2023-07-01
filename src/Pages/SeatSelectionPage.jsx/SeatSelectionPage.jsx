5903fdbb5552440c1ccacb34d1fca0fead44a4b9
mindytyj <derpbritz@gmail.com>
2023-07-02 03:57:40 +0800

    Amend Seat Selection Page to Include Components

--- src/Pages/SeatSelectionPage.jsx/SeatSelectionPage.jsx
+++ src/Pages/SeatSelectionPage.jsx/SeatSelectionPage.jsx
@@ -1,40 +1,16 @@
+import SeatLegend from "../../Components/SeatSelectionPage/SeatLegend";
+import TicketsTable from "../../Components/SeatSelectionPage/TicketsTable";
+import Stack from "@mui/material/Stack";
+
 export default function SeatSelectionPage() {
   return (
     <>
       <h1>SeatSelectionPage</h1>
-      <h3>Legend</h3>
-      <div>
-        <span>Available Seats</span>
-      </div>
-      <div>
-        <span>Selected Seats</span>
-      </div>
-      <div>
-        <span>On hold</span>
-      </div>
-      <div>
-        <span>Unavailable Seats</span>
-      </div>
-      <div>
-        <span>Wheel Chair Seats</span>
-      </div>
-      <table>
-        <thead>
-          <th>TYPE</th>
-          <th>QUANTITY</th>
-          <th>AMOUNT</th>
-        </thead>
-        <tbody>
-          <td>Seats: </td>
-          <td>Count</td>
-          <td>$10</td>
-        </tbody>
-        <tfoot>
-          <td>TOTAL</td>
-          <td> </td>
-          <td>$20</td>
-        </tfoot>
-      </table>
+      <Stack spacing={0}>
+        <SeatLegend />
+        <TicketsTable />
+      </Stack>
+
       <button>RESET</button>
       <button>CONFIRM SEAT(S)</button>
     </>
