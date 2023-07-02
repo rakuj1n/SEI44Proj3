40232ecebe1b30cb9c88da184a3a1fc44b1bc430
mindytyj <derpbritz@gmail.com>
2023-07-03 01:54:27 +0800

    Add Reset Button Functionality and Props to Tickets Table

--- src/Pages/SeatSelectionPage.jsx/SeatSelectionPage.jsx
+++ src/Pages/SeatSelectionPage.jsx/SeatSelectionPage.jsx
@@ -3,18 +3,34 @@ import SeatsSelection from "../../Components/SeatSelectionPage/SeatsSelection";
 import TicketsTable from "../../Components/SeatSelectionPage/TicketsTable";
 import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
+import { useEffect, useState } from "react";
 
 export default function SeatSelectionPage() {
+  const [seatSelection, setSeatSelection] = useState([]);
+  const [qty, setQty] = useState(0);
+  const [amount, setAmount] = useState(0);
+
+  useEffect(() => {}, [seatSelection, qty, amount]);
+
+  function handleReset() {
+    setSeatSelection([]);
+    setQty(0);
+    setAmount(0);
+  }
+
+  function handleConfirmedSeats() {
+    return;
+  }
+
   return (
     <Container maxWidth="ml">
-      <h1>SeatSelectionPage</h1>
       <Stack spacing={0}>
-        <SeatsSelection />
+        <SeatsSelection setSeatSelection={setSeatSelection} />
         <SeatsLegend />
-        <TicketsTable />
+        <TicketsTable seatSelection={seatSelection} qty={qty} amount={amount} />
       </Stack>
-      <button>RESET</button>
-      <button>CONFIRM SEAT(S)</button>
+      <button onClick={handleReset}>RESET</button>
+      <button onClick={handleConfirmedSeats}>CONFIRM SEAT(S)</button>
     </Container>
   );
 }
