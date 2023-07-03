e62866f103f31c611d08da024bbbf0cc432dd495
damienchoojl <damienchoojl@gmail.com>
2023-07-03 13:58:28 +0800

    updated Rent Button to direct to payment page

--- src/Components/MoviesPage/MoviesDetailsPage.jsx
+++ src/Components/MoviesPage/MoviesDetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -19,6 +19,7 @@ const MoviesDetailsPage = () => {
   const { title } = useParams();
   const [movie, setMovie] = useState(null);
   const [showTheatres, setShowTheatres] = useState(false);
+  const navigate = useNavigate();
 
   const fetchMovie = async (movieTitle) => {
     try {
@@ -49,11 +50,15 @@ const MoviesDetailsPage = () => {
 
   const handleBuyNow = () => {
     setShowTheatres(true);
-    console.log("Buy");
   };
 
   const handleRent = () => {
-    console.log("Rent");
+    navigate(`/payments`, {
+      state: {
+        movieTitle: movie.title,
+        poster: movie.poster,
+      },
+    });
   };
 
   const buyOrRentButton = movie.nowShowing ? (
@@ -62,7 +67,7 @@ const MoviesDetailsPage = () => {
     </button>
   ) : (
     <button className="rent-button" onClick={handleRent}>
-      Rent Movie Now
+      Rent S$4.99
     </button>
   );
 
