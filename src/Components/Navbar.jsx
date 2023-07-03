b430730721777c200f5bb5f77e5b5e30ab41ac85
damienchoojl <damienchoojl@gmail.com>
2023-07-03 15:00:20 +0800

    updated NavBar
    MyProfile and Settings will only appear if URL is not under Profile or Setting

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -41,19 +41,21 @@ export default function Navbar({ user, setUser }) {
                   <Link to="/mainpage">Home</Link>
                 </button>
               )}
-              {pathname !== `/users/${user._id}` && (
-                <button>
-                  <Link to={`/users/${user._id}`}>My Profile</Link>
-                </button>
-              )}
+              {pathname !== `/users/${user._id}` &&
+                pathname !== `/users/${user._id}/settings` && (
+                  <button>
+                    <Link to={`/users/${user._id}`}>My Profile</Link>
+                  </button>
+                )}
               <button>
                 <Link to={`/users/${user._id}/friends`}>My Friends</Link>
               </button>
-              {pathname !== `/users/${user._id}` && (
-                <button>
-                  <Link to={`/users/${user._id}/settings`}>Settings</Link>
-                </button>
-              )}
+              {pathname !== `/users/${user._id}` &&
+                pathname !== `/users/${user._id}/settings` && (
+                  <button>
+                    <Link to={`/users/${user._id}/settings`}>Settings</Link>
+                  </button>
+                )}
               <button>
                 <Link to="" onClick={handleLogOut}>
                   Log Out
